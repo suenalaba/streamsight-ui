120ef8f9e0100ce19188d67486653a0c2495e833
suenalaba <joshuajr13@gmail.com>
2024-08-28 22:23:52 +0800

    feat: implement get metric endpoint

--- server/main.py
+++ server/main.py
@@ -1,4 +1,5 @@
 import io
+import json
 from typing import List, Optional, Union, cast
 
 from fastapi import FastAPI, File, HTTPException, UploadFile
@@ -273,3 +274,36 @@ async def submit_prediction(evaluator_streamer_id: str, algorithm_id: str, file:
         return {"status": True}
     except Exception as e:
         return {"status": False, "error": f"Error Submitting Prediction: {str(e)}"}
+
+
+@app.get("/get_metrics/{evaluator_streamer_id}")
+def get_metrics(evaluator_streamer_id: str):
+    try:
+        evaluator_streamer_uuid = UUID(evaluator_streamer_id)
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Invalid UUID format")
+
+    evaluator_streamer: Optional[EvaluatorStreamer] = evaluator_stream_object_map.get(evaluator_streamer_uuid)
+    if not evaluator_streamer:
+        raise HTTPException(status_code=404, detail="EvaluatorStreamer not found")
+
+    evaluator_streamer = cast(EvaluatorStreamer, evaluator_streamer)
+
+    try:
+        micro_metrics = evaluator_streamer.metric_results("micro")
+        macro_metrics = evaluator_streamer.metric_results("macro")
+
+        micro_json = micro_metrics.to_json()
+        macro_json = macro_metrics.to_json()
+
+        # Combine both JSON strings into a dictionary
+        metrics_dict = {
+            "micro_metrics": json.loads(micro_json),
+            "macro_metrics": json.loads(macro_json)
+        }
+
+        # Convert the dictionary to a JSON string to send to the frontend
+        metrics_json = json.dumps(metrics_dict)
+        return metrics_json
+    except Exception as e:
+        return {"status": False, "error": f"Error Getting Metrics: {str(e)}"}
