833b6530c9730ca5fa6624d2f92252a42808834e
suenalaba <joshuajr13@gmail.com>
2024-09-05 12:44:08 +0800

    feat: improve exception handling for get all algo state

--- server/src/routers/algorithm_management.py
+++ server/src/routers/algorithm_management.py
@@ -65,13 +65,17 @@ def get_all_algorithm_state(stream_id: str):
     try:
         evaluator_streamer_uuid = UUID(stream_id)
     except ValueError:
-        raise HTTPException(status_code=400, detail="Invalid UUID format")
+        raise HTTPException(status_code=400, detail="Invalid Stream UUID format")
 
     evaluator_streamer: Optional[EvaluatorStreamer] = evaluator_stream_object_map.get(evaluator_streamer_uuid)
     if not evaluator_streamer:
         raise HTTPException(status_code=404, detail="EvaluatorStreamer not found")
 
     evaluator_streamer = cast(EvaluatorStreamer, evaluator_streamer)
-    algorithm_states = {key: value.name for key, value in evaluator_streamer.get_all_algorithm_status().items()}
+    
+    try:
+        algorithm_states = {key: value.name for key, value in evaluator_streamer.get_all_algorithm_status().items()}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error getting all algorithm states: {str(e)}")
+    
     return {"algorithm_states": algorithm_states}
-
