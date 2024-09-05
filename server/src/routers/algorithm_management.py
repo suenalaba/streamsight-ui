575e7e6c1033f28f615c3fb2a8dfc1d900a0c359
suenalaba <joshuajr13@gmail.com>
2024-09-05 12:19:59 +0800

    test: get algorithm state endpoint

--- server/src/routers/algorithm_management.py
+++ server/src/routers/algorithm_management.py
@@ -39,19 +39,24 @@ def get_algorithm_state(stream_id: str, algorithm_id: str):
     try:
         evaluator_streamer_uuid = UUID(stream_id)
     except ValueError:
-        raise HTTPException(status_code=400, detail="Invalid UUID format")
+        raise HTTPException(status_code=400, detail="Invalid Stream UUID format")
     
     try:
         algorithm_uuid = UUID(algorithm_id)
     except ValueError:
-        raise HTTPException(status_code=400, detail="Invalid UUID format")
+        raise HTTPException(status_code=400, detail="Invalid Algorithm UUID format")
 
     evaluator_streamer: Optional[EvaluatorStreamer] = evaluator_stream_object_map.get(evaluator_streamer_uuid)
     if not evaluator_streamer:
         raise HTTPException(status_code=404, detail="EvaluatorStreamer not found")
 
     evaluator_streamer = cast(EvaluatorStreamer, evaluator_streamer)
-    algorithm_state = evaluator_streamer.get_algorithm_state(algorithm_uuid).name
+
+    try:
+        algorithm_state = evaluator_streamer.get_algorithm_state(algorithm_uuid).name
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error getting algorithm state: {str(e)}")
+    
     return {"algorithm_state": algorithm_state}
 
 
