13d73f9ab48822106d4e80103d002875018a58c9
suenalaba <joshuajr13@gmail.com>
2024-09-16 21:08:44 +0800

    feat: has next data endpoint

--- server/src/routers/algorithm_management.py
+++ server/src/routers/algorithm_management.py
@@ -79,3 +79,27 @@ def get_all_algorithm_state(stream_id: str):
         raise HTTPException(status_code=500, detail=f"Error getting all algorithm states: {str(e)}")
     
     return {"algorithm_states": algorithm_states}
+
+@router.get("streams/{stream_id}/algorithms/{algorithm_id}/is-completed")
+def is_algorithm_streaming_completed(stream_id: str, algorithm_id: str):
+    try:
+        evaluator_streamer_uuid = UUID(stream_id)
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Invalid Stream UUID format")
+    
+    try:
+        algorithm_uuid = UUID(algorithm_id)
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Invalid Algorithm UUID format")
+
+    evaluator_streamer: Optional[EvaluatorStreamer] = evaluator_stream_object_map.get(evaluator_streamer_uuid)
+    if not evaluator_streamer:
+        raise HTTPException(status_code=404, detail="EvaluatorStreamer not found")
+
+    evaluator_streamer = cast(EvaluatorStreamer, evaluator_streamer)
+
+    try:
+        algorithm_state = evaluator_streamer.get_algorithm_state(algorithm_uuid).name
+        return algorithm_state == "COMPLETED"
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error checking if algorithm streaming is completed: {str(e)}")
