b807fe1cca447e98e10a84919e78b5a60eeb5fcf
Josh <61338348+suenalaba@users.noreply.github.com>
2024-09-21 12:34:26 +0800

    feat: allow csr matrix predictions (#94)

--- server/src/routers/predictions.py
+++ server/src/routers/predictions.py
@@ -1,8 +1,9 @@
-from typing import List, cast
+from typing import List, Union, cast
 from uuid import UUID
 from fastapi import APIRouter, HTTPException
 import pandas as pd
 from pydantic import BaseModel, Field
+from scipy.sparse import csr_matrix
 from streamsight.evaluators.evaluator_stream import EvaluatorStreamer
 from streamsight.matrix import InteractionMatrix
 from src.db_utils import get_evaluator_stream_from_db, update_evaluator_stream
@@ -11,6 +12,12 @@ router = APIRouter(
   tags=["Predictions"]
 )
 
+class PredictionCsrMatrix(BaseModel):
+    data: List[float]
+    indices: List[int]
+    indptr: List[int]
+    shape: List[int]
+
 class DataframeRecord(BaseModel):
     interactionid: int = Field(..., description="The ID of the interaction, required.")
     uid: int = Field(..., description="The user ID, required.")
@@ -18,7 +25,7 @@ class DataframeRecord(BaseModel):
     ts: int = Field(..., description="The timestamp of the interaction, required.")
 
 @router.post("/streams/{stream_id}/algorithms/{algorithm_id}/predictions")
-async def submit_prediction(stream_id: str, algorithm_id: str, records: List[DataframeRecord]):
+async def submit_prediction(stream_id: str, algorithm_id: str, predictions: Union[List[DataframeRecord], PredictionCsrMatrix]):
     try:
         evaluator_streamer_uuid = UUID(stream_id)
     except ValueError:
@@ -39,15 +46,27 @@ async def submit_prediction(stream_id: str, algorithm_id: str, records: List[Dat
 
     evaluator_streamer = cast(EvaluatorStreamer, evaluator_streamer)
 
-    try:
-        prediction_data = [record.model_dump() for record in records]
-        prediction_df = pd.DataFrame(prediction_data)
-        prediction_im = InteractionMatrix(prediction_df, item_ix='iid', user_ix='uid', timestamp_ix='ts')
-
-        evaluator_streamer.submit_prediction(algorithm_uuid, prediction_im)
-        assert evaluator_streamer.get_algorithm_state(algorithm_uuid).name in {"PREDICTED", "COMPLETED"}
-        update_evaluator_stream(evaluator_streamer_uuid, evaluator_streamer)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error Submitting Prediction: {str(e)}")
     
+    if isinstance(predictions, list) and all(isinstance(prediction, DataframeRecord) for prediction in predictions):
+        try:
+            prediction_data = [prediction.model_dump() for prediction in predictions]
+            prediction_df = pd.DataFrame(prediction_data)
+            prediction_im = InteractionMatrix(prediction_df, item_ix='iid', user_ix='uid', timestamp_ix='ts')
+
+            evaluator_streamer.submit_prediction(algorithm_uuid, prediction_im)
+            assert evaluator_streamer.get_algorithm_state(algorithm_uuid).name in {"PREDICTED", "COMPLETED"}
+            update_evaluator_stream(evaluator_streamer_uuid, evaluator_streamer)
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Error Submitting Prediction: {str(e)}")
+    elif isinstance(predictions, PredictionCsrMatrix):
+        try:
+            prediction_csr_matrix = csr_matrix((predictions.data, predictions.indices, predictions.indptr), shape=predictions.shape)
+            evaluator_streamer.submit_prediction(algorithm_uuid, prediction_csr_matrix)
+            assert evaluator_streamer.get_algorithm_state(algorithm_uuid).name in {"PREDICTED", "COMPLETED"}
+            update_evaluator_stream(evaluator_streamer_uuid, evaluator_streamer)
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Error Submitting Prediction: {str(e)}")
+    else:
+        raise HTTPException(status_code=400, detail="Prediction data must be a list of DataframeRecord or PredictionCsrMatrix")
+
     return {"status": True}
