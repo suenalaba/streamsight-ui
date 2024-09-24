1e33fc5b587cb77fa466fe4238bef858bf1ddd0e
Josh <61338348+suenalaba@users.noreply.github.com>
2024-09-24 17:46:20 +0800

    test: get unlabeled data endpoint (#128)

--- server/src/routers/data_handling.py
+++ server/src/routers/data_handling.py
@@ -6,9 +6,8 @@ from streamsight.evaluators.evaluator_stream import EvaluatorStreamer
 
 from src.db_utils import get_evaluator_stream_from_db, update_evaluator_stream
 
-router = APIRouter(
-  tags=["Data Handling"]
-)
+router = APIRouter(tags=["Data Handling"])
+
 
 @router.get("/streams/{stream_id}/algorithms/{algorithm_id}/training-data")
 def get_training_data(stream_id: str, algorithm_id: str):
@@ -16,12 +15,12 @@ def get_training_data(stream_id: str, algorithm_id: str):
         evaluator_streamer_uuid = UUID(stream_id)
     except ValueError:
         raise HTTPException(status_code=400, detail="Invalid UUID format")
-    
+
     try:
         algorithm_uuid = UUID(algorithm_id)
     except ValueError:
         raise HTTPException(status_code=400, detail="Invalid UUID format")
-    
+
     try:
         evaluator_streamer = get_evaluator_stream_from_db(evaluator_streamer_uuid)
     except Exception as e:
@@ -30,15 +29,17 @@ def get_training_data(stream_id: str, algorithm_id: str):
         raise HTTPException(status_code=404, detail="EvaluatorStreamer not found")
 
     evaluator_streamer = cast(EvaluatorStreamer, evaluator_streamer)
-    
+
     try:
         interaction_matrix = evaluator_streamer.get_data(algorithm_uuid)
         shape = interaction_matrix.shape
         df = interaction_matrix.copy_df()
-        df_json = df.to_dict(orient='records')
+        df_json = df.to_dict(orient="records")
         update_evaluator_stream(evaluator_streamer_uuid, evaluator_streamer)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error Getting Training Data: {str(e)}")
+        raise HTTPException(
+            status_code=500, detail=f"Error Getting Training Data: {str(e)}"
+        )
 
     return {"shape": shape, "training_data": df_json}
 
@@ -49,31 +50,33 @@ def get_unlabeled_data(stream_id: str, algorithm_id: str):
         evaluator_streamer_uuid = UUID(stream_id)
     except ValueError:
         raise HTTPException(status_code=400, detail="Invalid UUID format")
-    
+
     try:
         algorithm_uuid = UUID(algorithm_id)
     except ValueError:
         raise HTTPException(status_code=400, detail="Invalid UUID format")
-    
+
     try:
         evaluator_streamer = get_evaluator_stream_from_db(evaluator_streamer_uuid)
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error Getting Stream: {str(e)}")
-    
+
     if not evaluator_streamer:
         raise HTTPException(status_code=404, detail="EvaluatorStreamer not found")
 
     evaluator_streamer = cast(EvaluatorStreamer, evaluator_streamer)
-    
+
     try:
         interaction_matrix = evaluator_streamer.get_unlabeled_data(algorithm_uuid)
         shape = interaction_matrix.shape
         df = interaction_matrix.copy_df()
-        df_json = df.to_dict(orient='records')
+        df_json = df.to_dict(orient="records")
 
         update_evaluator_stream(evaluator_streamer_uuid, evaluator_streamer)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error Getting Training Data: {str(e)}")
+        raise HTTPException(
+            status_code=500, detail=f"Error Getting Unlabeled Data: {str(e)}"
+        )
 
     return {"shape": shape, "unlabeled_data": df_json}
