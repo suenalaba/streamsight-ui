c3e00d3b1764f06b08f6253301bdd764201e9ae4
Josh <61338348+suenalaba@users.noreply.github.com>
2024-09-26 12:29:57 +0800

    refactor: test_main.py (#144)

--- server/tests/test_main.py
+++ server/tests/test_main.py
@@ -1,10 +1,14 @@
+import unittest
+
 from fastapi.testclient import TestClient
 
 from src.main import app
 
 client = TestClient(app)
 
-def test_healthcheck():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"Server is running, STATUS": "HEALTHY"}
+
+class TestMain(unittest.TestCase):
+    def test_healthcheck():
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.json() == {"Server is running, STATUS": "HEALTHY"}
