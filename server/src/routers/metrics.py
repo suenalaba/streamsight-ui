fc9a386fe2dd63567c910f1e08e13c3e9af7ed75
suenalaba <joshuajr13@gmail.com>
2024-10-01 16:46:29 +0800

    refactor: get metrics endpoint

--- server/src/routers/metrics.py
+++ server/src/routers/metrics.py
@@ -1,34 +1,25 @@
 import json
-from typing import cast
-from uuid import UUID
 
 from fastapi import APIRouter, HTTPException
-from streamsight.evaluators.evaluator_stream import EvaluatorStreamer
 
-from src.db_utils import get_evaluator_stream_from_db, update_evaluator_stream
+from src.utils.db_utils import (
+    DatabaseErrorException,
+    GetEvaluatorStreamErrorException,
+    get_stream_from_db,
+    update_stream,
+)
+from src.utils.uuid_utils import InvalidUUIDException, get_stream_uuid_object
 
 router = APIRouter(
-  tags=["Metrics"],
+    tags=["Metrics"],
 )
+
 
 @router.get("/streams/{stream_id}/metrics")
 def get_metrics(stream_id: str):
     try:
-        evaluator_streamer_uuid = UUID(stream_id)
-    except ValueError:
-        raise HTTPException(status_code=400, detail="Invalid UUID format")
-
-    try:
-        evaluator_streamer = get_evaluator_stream_from_db(evaluator_streamer_uuid)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error Getting Stream: {str(e)}")
-
-    if not evaluator_streamer:
-        raise HTTPException(status_code=404, detail="EvaluatorStreamer not found")
-
-    evaluator_streamer = cast(EvaluatorStreamer, evaluator_streamer)
-
-    try:
+        evaluator_streamer_uuid = get_stream_uuid_object(stream_id)
+        evaluator_streamer = get_stream_from_db(evaluator_streamer_uuid)
         micro_metrics = evaluator_streamer.metric_results("micro")
         macro_metrics = evaluator_streamer.metric_results("macro")
 
@@ -37,14 +28,19 @@ def get_metrics(stream_id: str):
 
         metrics_dict = {
             "micro_metrics": json.loads(micro_json),
-            "macro_metrics": json.loads(macro_json)
+            "macro_metrics": json.loads(macro_json),
         }
 
         metrics_json = json.dumps(metrics_dict)
-        
-        update_evaluator_stream(evaluator_streamer_uuid, evaluator_streamer)
-        
+
+        update_stream(evaluator_streamer_uuid, evaluator_streamer)
+
         return metrics_json
+    except (
+        InvalidUUIDException,
+        GetEvaluatorStreamErrorException,
+        DatabaseErrorException,
+    ) as e:
+        raise HTTPException(status_code=e.status_code, detail=e.message)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error Getting Metrics: {str(e)}")
-    
