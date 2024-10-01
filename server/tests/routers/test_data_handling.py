fd51ff40997a49780addd551811c79c521e703fe
Josh <61338348+suenalaba@users.noreply.github.com>
2024-10-01 15:13:52 +0800

    refactor: get training data endpoint (#164)

--- server/tests/routers/test_data_handling.py
+++ server/tests/routers/test_data_handling.py
@@ -6,6 +6,8 @@ import pandas as pd
 from fastapi.testclient import TestClient
 
 from src.main import app
+from src.utils.db_utils import DatabaseErrorException, GetEvaluatorStreamErrorException
+from src.utils.uuid_utils import InvalidUUIDException
 
 client = TestClient(app)
 
@@ -43,15 +45,27 @@ class TestGetTrainingData(unittest.TestCase):
 
     def test_get_training_data_valid(self):
         with patch(
-            "src.routers.data_handling.get_evaluator_stream_from_db",
+            "src.routers.data_handling.get_stream_uuid_object",
+            return_value=UUID("336e4cb7-861b-4870-8c29-3ffc530711ef"),
+        ) as mock_get_stream_uuid, patch(
+            "src.routers.data_handling.get_algo_uuid_object",
+            return_value=UUID("12345678-1234-5678-1234-567812345678"),
+        ) as mock_get_algo_uuid, patch(
+            "src.routers.data_handling.get_stream_from_db",
             return_value=self.mock_evaluator_streamer,
         ) as mock_get_evaluator_stream_from_db, patch(
-            "src.routers.data_handling.update_evaluator_stream", return_value=None
+            "src.routers.data_handling.update_stream", return_value=None
         ) as mock_update_evaluator_stream:
             response = client.get(
                 "/streams/336e4cb7-861b-4870-8c29-3ffc530711ef/algorithms/12345678-1234-5678-1234-567812345678/training-data"
             )
 
+            mock_get_stream_uuid.assert_called_once_with(
+                "336e4cb7-861b-4870-8c29-3ffc530711ef"
+            )
+            mock_get_algo_uuid.assert_called_once_with(
+                "12345678-1234-5678-1234-567812345678"
+            )
             mock_get_evaluator_stream_from_db.assert_called_once_with(
                 UUID("336e4cb7-861b-4870-8c29-3ffc530711ef")
             )
@@ -76,65 +90,148 @@ class TestGetTrainingData(unittest.TestCase):
             }
 
     def test_get_training_data_invalid_stream_uuid(self):
-        response = client.get(
-            "/streams/invalid-stream-uuid/algorithms/12345678-1234-5678-1234-567812345678/training-data"
-        )
-        assert response.status_code == 400
-        assert response.json() == {"detail": "Invalid UUID format"}
+        with patch(
+            "src.routers.data_handling.get_stream_uuid_object",
+            side_effect=InvalidUUIDException("Invalid UUID format"),
+        ) as mock_get_stream_uuid, patch(
+            "src.routers.data_handling.get_algo_uuid_object",
+            return_value=UUID("12345678-1234-5678-1234-567812345678"),
+        ) as mock_get_algo_uuid, patch(
+            "src.routers.data_handling.get_stream_from_db",
+            return_value=self.mock_evaluator_streamer,
+        ) as mock_get_evaluator_stream_from_db, patch(
+            "src.routers.data_handling.update_stream", return_value=None
+        ) as mock_update_evaluator_stream:
+            response = client.get(
+                "/streams/invalid-stream-uuid/algorithms/12345678-1234-5678-1234-567812345678/training-data"
+            )
+
+            mock_get_stream_uuid.assert_called_once_with("invalid-stream-uuid")
+            mock_get_algo_uuid.assert_not_called()
+            mock_get_evaluator_stream_from_db.assert_not_called()
+            mock_update_evaluator_stream.assert_not_called()
+
+            assert response.status_code == 400
+            assert response.json() == {"detail": "Invalid UUID format"}
 
     def test_get_training_data_invalid_algo_uuid(self):
-        response = client.get(
-            "/streams/336e4cb7-861b-4870-8c29-3ffc530711ef/algorithms/invalid-algo-uuid/training-data"
-        )
-        assert response.status_code == 400
-        assert response.json() == {"detail": "Invalid UUID format"}
+        with patch(
+            "src.routers.data_handling.get_stream_uuid_object",
+            return_value=UUID("336e4cb7-861b-4870-8c29-3ffc530711ef"),
+        ) as mock_get_stream_uuid, patch(
+            "src.routers.data_handling.get_algo_uuid_object",
+            side_effect=InvalidUUIDException("Invalid UUID format"),
+        ) as mock_get_algo_uuid, patch(
+            "src.routers.data_handling.get_stream_from_db",
+            return_value=self.mock_evaluator_streamer,
+        ) as mock_get_evaluator_stream_from_db, patch(
+            "src.routers.data_handling.update_stream", return_value=None
+        ) as mock_update_evaluator_stream:
+            response = client.get(
+                "/streams/336e4cb7-861b-4870-8c29-3ffc530711ef/algorithms/invalid-algo-uuid/training-data"
+            )
+
+            mock_get_stream_uuid.assert_called_once_with(
+                "336e4cb7-861b-4870-8c29-3ffc530711ef"
+            )
+            mock_get_algo_uuid.assert_called_once_with("invalid-algo-uuid")
+            mock_get_evaluator_stream_from_db.assert_not_called()
+            mock_update_evaluator_stream.assert_not_called()
+
+            assert response.status_code == 400
+            assert response.json() == {"detail": "Invalid UUID format"}
 
     def test_get_training_data_stream_not_found(self):
         with patch(
-            "src.routers.data_handling.get_evaluator_stream_from_db", return_value=None
-        ) as mock_get_evaluator_stream_from_db:
+            "src.routers.data_handling.get_stream_uuid_object",
+            return_value=UUID("336e4cb7-861b-4870-8c29-3ffc530711ef"),
+        ) as mock_get_stream_uuid, patch(
+            "src.routers.data_handling.get_algo_uuid_object",
+            return_value=UUID("12345678-1234-5678-1234-567812345678"),
+        ) as mock_get_algo_uuid, patch(
+            "src.routers.data_handling.get_stream_from_db",
+            side_effect=GetEvaluatorStreamErrorException(
+                message="EvaluatorStreamer not found", status_code=404
+            ),
+        ) as mock_get_evaluator_stream_from_db, patch(
+            "src.routers.data_handling.update_stream", return_value=None
+        ) as mock_update_evaluator_stream:
             response = client.get(
                 "/streams/336e4cb7-861b-4870-8c29-3ffc530711ef/algorithms/12345678-1234-5678-1234-567812345678/training-data"
             )
 
+            mock_get_stream_uuid.assert_called_once_with(
+                "336e4cb7-861b-4870-8c29-3ffc530711ef"
+            )
+            mock_get_algo_uuid.assert_called_once_with(
+                "12345678-1234-5678-1234-567812345678"
+            )
             mock_get_evaluator_stream_from_db.assert_called_once_with(
                 UUID("336e4cb7-861b-4870-8c29-3ffc530711ef")
             )
             self.mock_evaluator_streamer.get_data.assert_not_called()
+            mock_update_evaluator_stream.assert_not_called()
 
             assert response.status_code == 404
             assert response.json() == {"detail": "EvaluatorStreamer not found"}
 
     def test_get_training_get_stream_error(self):
         with patch(
-            "src.routers.data_handling.get_evaluator_stream_from_db",
-            side_effect=Exception("Error getting stream"),
-        ) as mock_get_evaluator_stream_from_db:
-            response = client.get(
-                "/streams/336e4cb7-861b-4870-8c29-3ffc530711ef/algorithms/12345678-1234-5678-1234-567812345678/training-data"
-            )
-
-            mock_get_evaluator_stream_from_db.assert_called_once_with(
-                UUID("336e4cb7-861b-4870-8c29-3ffc530711ef")
-            )
-            self.mock_evaluator_streamer.get_data.assert_not_called()
-
-            assert response.status_code == 500
-            assert response.json() == {
-                "detail": "Error Getting Stream: Error getting stream"
-            }
-
-    def test_get_training_data_error(self):
-        with patch(
-            "src.routers.data_handling.get_evaluator_stream_from_db",
-            return_value=self.mock_evaluator_streamer_error,
+            "src.routers.data_handling.get_stream_uuid_object",
+            return_value=UUID("336e4cb7-861b-4870-8c29-3ffc530711ef"),
+        ) as mock_get_stream_uuid, patch(
+            "src.routers.data_handling.get_algo_uuid_object",
+            return_value=UUID("12345678-1234-5678-1234-567812345678"),
+        ) as mock_get_algo_uuid, patch(
+            "src.routers.data_handling.get_stream_from_db",
+            side_effect=GetEvaluatorStreamErrorException(),
         ) as mock_get_evaluator_stream_from_db, patch(
-            "src.routers.data_handling.update_evaluator_stream", return_value=None
+            "src.routers.data_handling.update_stream", return_value=None
         ) as mock_update_evaluator_stream:
             response = client.get(
                 "/streams/336e4cb7-861b-4870-8c29-3ffc530711ef/algorithms/12345678-1234-5678-1234-567812345678/training-data"
             )
 
+            mock_get_stream_uuid.assert_called_once_with(
+                "336e4cb7-861b-4870-8c29-3ffc530711ef"
+            )
+            mock_get_algo_uuid.assert_called_once_with(
+                "12345678-1234-5678-1234-567812345678"
+            )
+            mock_get_evaluator_stream_from_db.assert_called_once_with(
+                UUID("336e4cb7-861b-4870-8c29-3ffc530711ef")
+            )
+            self.mock_evaluator_streamer.get_data.assert_not_called()
+            mock_update_evaluator_stream.assert_not_called()
+
+            assert response.status_code == 500
+            assert response.json() == {
+                "detail": "Error getting evaluator stream from database"
+            }
+
+    def test_get_training_data_error(self):
+        with patch(
+            "src.routers.data_handling.get_stream_uuid_object",
+            return_value=UUID("336e4cb7-861b-4870-8c29-3ffc530711ef"),
+        ) as mock_get_stream_uuid, patch(
+            "src.routers.data_handling.get_algo_uuid_object",
+            return_value=UUID("12345678-1234-5678-1234-567812345678"),
+        ) as mock_get_algo_uuid, patch(
+            "src.routers.data_handling.get_stream_from_db",
+            return_value=self.mock_evaluator_streamer_error,
+        ) as mock_get_evaluator_stream_from_db, patch(
+            "src.routers.data_handling.update_stream", return_value=None
+        ) as mock_update_evaluator_stream:
+            response = client.get(
+                "/streams/336e4cb7-861b-4870-8c29-3ffc530711ef/algorithms/12345678-1234-5678-1234-567812345678/training-data"
+            )
+
+            mock_get_stream_uuid.assert_called_once_with(
+                "336e4cb7-861b-4870-8c29-3ffc530711ef"
+            )
+            mock_get_algo_uuid.assert_called_once_with(
+                "12345678-1234-5678-1234-567812345678"
+            )
             mock_get_evaluator_stream_from_db.assert_called_once_with(
                 UUID("336e4cb7-861b-4870-8c29-3ffc530711ef")
             )
@@ -147,6 +244,44 @@ class TestGetTrainingData(unittest.TestCase):
             assert response.json() == {
                 "detail": "Error Getting Training Data: Error getting training data"
             }
+
+    def test_get_training_data_error_updating_stream(self):
+        with patch(
+            "src.routers.data_handling.get_stream_uuid_object",
+            return_value=UUID("336e4cb7-861b-4870-8c29-3ffc530711ef"),
+        ) as mock_get_stream_uuid, patch(
+            "src.routers.data_handling.get_algo_uuid_object",
+            return_value=UUID("12345678-1234-5678-1234-567812345678"),
+        ) as mock_get_algo_uuid, patch(
+            "src.routers.data_handling.get_stream_from_db",
+            return_value=self.mock_evaluator_streamer,
+        ) as mock_get_evaluator_stream_from_db, patch(
+            "src.routers.data_handling.update_stream",
+            side_effect=DatabaseErrorException("error updating db"),
+        ) as mock_update_evaluator_stream:
+            response = client.get(
+                "/streams/336e4cb7-861b-4870-8c29-3ffc530711ef/algorithms/12345678-1234-5678-1234-567812345678/training-data"
+            )
+
+            mock_get_stream_uuid.assert_called_once_with(
+                "336e4cb7-861b-4870-8c29-3ffc530711ef"
+            )
+            mock_get_algo_uuid.assert_called_once_with(
+                "12345678-1234-5678-1234-567812345678"
+            )
+            mock_get_evaluator_stream_from_db.assert_called_once_with(
+                UUID("336e4cb7-861b-4870-8c29-3ffc530711ef")
+            )
+            self.mock_evaluator_streamer.get_data.assert_called_once_with(
+                UUID("12345678-1234-5678-1234-567812345678")
+            )
+            mock_update_evaluator_stream.assert_called_once_with(
+                UUID("336e4cb7-861b-4870-8c29-3ffc530711ef"),
+                self.mock_evaluator_streamer,
+            )
+
+            assert response.status_code == 500
+            assert response.json() == {"detail": "error updating db"}
 
 
 class TestGetUnlabeledData(unittest.TestCase):
