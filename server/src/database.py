8efed01580812dd5ce0c309d65f41ad1cea5ca84
suenalaba <joshuajr13@gmail.com>
2024-10-08 22:25:24 +0800

    feat: integrate supabase db

--- server/src/database.py
+++ server/src/database.py
@@ -1,22 +1,32 @@
+import os
 import uuid
 
 from sqlalchemy import Engine
 from sqlmodel import Field, SQLModel, create_engine
 
+# Specify the path to the .env file
+dotenv_path = os.path.join(os.path.dirname(__file__), "..", ".env")
+
 
 class EvaluatorStreamModel(SQLModel, table=True):
-    __tablename__ = "evaluator_streams"
+    __tablename__ = "streams"
     stream_id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     stream_object: bytes
 
 
 # SQL Connection
 _engine: Engine = None
+user = os.getenv("user")
+password = os.getenv("password")
+host = os.getenv("host")
+port = os.getenv("port")
+dbname = os.getenv("dbname")
+connection_string = f"postgresql://{user}:{password}@{host}:{port}/{dbname}"
 
 
 def get_sql_connection() -> Engine:
     global _engine
     if _engine is None:
-        _engine = create_engine("postgresql://localhost:5432/hero")
+        _engine = create_engine(connection_string)
         SQLModel.metadata.create_all(_engine)
     return _engine
