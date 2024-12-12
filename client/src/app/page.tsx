c79f806d845390ac8670814749a586413d40b445
Josh <61338348+suenalaba@users.noreply.github.com>
2024-12-12 16:29:24 +0800

    feat: create stream form (#219)

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,13 @@
+'use client';
+
+import Form from '@/components/Form/Form';
 import { Welcome } from '@/components/Welcome/Welcome';
 
 export default function Home() {
   return (
     <>
       <Welcome />
+      <Form/>
     </>
   );
 }
