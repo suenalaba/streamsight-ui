f9e7abb41f74103623200b0d5b7824b0e3e6c9af
suenalaba <joshuajr13@gmail.com>
2024-12-15 17:55:23 +0800

    feat: integrate server and client

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,30 @@
 'use client';
 
+import { getStatus } from '@/api';
 import { Welcome } from '@/components/Welcome/Welcome';
+import { Text } from '@mantine/core';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [status, setStatus] = useState('loading');
+
+  const fetchStatus = async () => {
+    try {
+      await getStatus();
+      setStatus('success');
+    } catch (error) {
+      setStatus('error');
+    }
+  }
+
+  useEffect(() => {
+    fetchStatus();
+  },[]);
+
   return (
     <>
       <Welcome />
+      <Text>{`Healthcheck status: ${status}`}</Text>
     </>
   );
 }
