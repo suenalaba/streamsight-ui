386e68391e8e38c7f8e39947cdc0f89a44372584
suenalaba <joshuajr13@gmail.com>
2025-02-05 20:34:07 +0800

    feat: datasets page

---
+++ client/src/components/Cards/DatasetPageCard.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Card, CardSection, Flex, Image, Text, Title } from '@mantine/core';
+
+interface DatasetPageCardProps {
+  title: string;
+  description: string;
+  imgSrc: string;
+  imgAlt: string;
+  href: string;
+}
+
+const DatasetPageCard = ({ title, description, imgSrc, imgAlt, href }: DatasetPageCardProps) => {
+  return (
+    <Card shadow="sm" padding="lg" radius="md" withBorder component="a" href={href} target="_blank">
+      <CardSection>
+        <Image src={imgSrc} height={160} alt={imgAlt} />
+      </CardSection>
+      <Flex align="flex-start" wrap="wrap" gap="md">
+        <Flex direction="column" style={{ flex: 1, minWidth: 0 }}>
+          <Title order={5} mb={10}>
+            {title}
+          </Title>
+          <Text size="xs">{description}</Text>
+        </Flex>
+      </Flex>
+    </Card>
+  );
+};
+
+export default DatasetPageCard;
