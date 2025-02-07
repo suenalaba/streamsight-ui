792738112085bdf0f859805adc91911234ae00e2
suenalaba <joshuajr13@gmail.com>
2025-02-07 14:58:12 +0800

    feat: setup e2e test on playwright

--- server/src/main.py
+++ server/src/main.py
@@ -17,6 +17,8 @@ origins = [
     "http://localhost:3000",
     "http://localhost:3001",
     "http://localhost",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:3001",
 ]
 
 app.add_middleware(
