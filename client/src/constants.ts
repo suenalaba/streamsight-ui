3a7da3f71d0070af2789f2602f0b75eb804fdad1
Josh <61338348+suenalaba@users.noreply.github.com>
2025-02-11 17:10:43 +0800

    chore: update server url (#313)

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -2,20 +2,20 @@ import { StreamStatusEnum } from '@/enum';
 
 export const getStatusBadgeProps = (status: StreamStatusEnum) => {
   const StatusToBadgePropsMappings = {
-    "NOT_STARTED": {
+    NOT_STARTED: {
       color: 'gray',
       label: StreamStatusEnum.NOT_STARTED,
     },
-    "IN_PROGRESS": {
+    IN_PROGRESS: {
       color: 'blue',
       label: StreamStatusEnum.IN_PROGRESS,
     },
-    "COMPLETED": {
+    COMPLETED: {
       color: 'lime',
       label: StreamStatusEnum.COMPLETED,
     },
-  }
+  };
   return StatusToBadgePropsMappings[status];
-}
+};
 
-export const BASE_URL = 'http://127.0.0.1:8000';
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://127.0.0.1:8000';
